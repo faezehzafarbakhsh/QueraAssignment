a57cee700376a4212eedda9ffc2e74c0cd163464
Afshin <khodeafshin@gmail.com>
2023-11-04 21:13:45 +0330

    [Add] models & variable_names

---
+++ EduRequest/variable_names.py
@@ -0,0 +1,15 @@
+# StudentRequest : 
+
+STUDENT = 'دانشجو'
+TERM = 'ترم'
+COURSE_TERM = 'درس ترمی'
+REQUEST_DESCRIPTION = 'توضیحات درخواست'
+ANSWER = 'پاسخ'
+STATUS = None
+HAS_ACADEMIC_YEAR = 'دارای سنوات تحصیلی'
+USER_ANSWER = 'توضیحات معاون آموزشی'
+REQUEST_TYPE = None
+
+
+# EnrollmentCertificate
+ENROLLMENT_CERTIFICATE_PLACE = None
