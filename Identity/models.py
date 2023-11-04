7f2c41c88cd54e080d16969708329cea921ec0b4
Ali Safari <alisfryly075@gmail.com>
2023-11-04 22:12:22 +0330

    [fix] models file

--- Identity/models.py
+++ Identity/models.py
@@ -4,7 +4,7 @@ from django.db import models
 import variable_names as vn_identity 
 from django_jalali.db import models as jmodels
 from django.contrib.auth import get_user_model
-from django.utils.translation import gettext_lazy as _
+
 # Create your user_models here.
 
 
@@ -15,25 +15,25 @@ class UserManager(models.Manager):
 class User(user_models.AbstractUser):
     
     class GenderChoices(models.IntegerChoices):
-        MALE = 1, _(vn_identity.MALE)
-        FEMALE = 2, _(vn_identity.FEMALE)
+        MALE = 1, (vn_identity.MALE)
+        FEMALE = 2, (vn_identity.FEMALE)
         
     class MilitaryChoices(models.IntegerChoices):
-        DUTIABLE  = 1, _(vn_identity.DUTIABLE)
-        EXEMPT = 2, _(vn_identity.EXEMPT)
+        DUTIABLE  = 1, (vn_identity.DUTIABLE)
+        EXEMPT = 2, (vn_identity.EXEMPT)
 
-    portrait = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name=_(vn_identity.PORTRAIT))
-    mobile = models.CharField(max_length=11, null=True, blank=True, verbose_name=_(vn_identity.MOBILE))
-    gender = models.IntegerField(choices=GenderChoices.choices, null=True, blank=True, verbose_name=_(vn_identity.GENDER))
-    birth_date = jmodels.jDateField(verbose_name=_(vn_identity.BIRTH_DATE)) 
-    college_id = models.ForeignKey('EduBase.College', on_delete=models.PROTECT, verbose_name=_(vn_identity.COLLEGE_ID))  
-    edu_field_id = models.ForeignKey('EduBase.EducField', on_delete=models.PROTECT, verbose_name=_(vn_identity.EDU_FIELD_ID))  
-    is_it_manager = models.BooleanField(default=False, verbose_name=_(vn_identity.IS_IT_MANAGER))  
-    is_chancellor = models.BooleanField(default=False, verbose_name=_(vn_identity.IS_CHANCELLOR))  
-    national_code = models.CharField(max_length=10, null=True, blank=True, verbose_name=_(vn_identity.NATIONAL_CODE))
-    is_student = models.BooleanField(default=False, verbose_name=_(vn_identity.IS_STUDENT))
-    is_teacher = models.BooleanField(default=False, verbose_name=_(vn_identity.IS_TEACHER))
-    military_service = models.IntegerField(choices=MilitaryChoices.choices, null=True, blank=True, verbose_name=_(vn_identity.MILITARY_SERVICE))
+    portrait = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name=(vn_identity.PORTRAIT))
+    mobile = models.CharField(max_length=11, null=True, blank=True, verbose_name=(vn_identity.MOBILE))
+    gender = models.IntegerField(choices=GenderChoices.choices, null=True, blank=True, verbose_name=(vn_identity.GENDER))
+    birth_date = jmodels.jDateField(verbose_name=(vn_identity.BIRTH_DATE)) 
+    college = models.ForeignKey('EduBase.College', on_delete=models.PROTECT, verbose_name=(vn_identity.COLLEGE_ID))  
+    edu_field = models.ForeignKey('EduBase.EduField', on_delete=models.PROTECT, verbose_name=(vn_identity.EDU_FIELD_ID))  
+    is_it_manager = models.BooleanField(default=False, verbose_name=(vn_identity.IS_IT_MANAGER))  
+    is_chancellor = models.BooleanField(default=False, verbose_name=(vn_identity.IS_CHANCELLOR))  
+    national_code = models.CharField(max_length=10, null=True, blank=True, verbose_name=(vn_identity.NATIONAL_CODE))
+    is_student = models.BooleanField(default=False, verbose_name=(vn_identity.IS_STUDENT))
+    is_teacher = models.BooleanField(default=False, verbose_name=(vn_identity.IS_TEACHER))
+    military_service = models.IntegerField(choices=MilitaryChoices.choices, null=True, blank=True, verbose_name=(vn_identity.MILITARY_SERVICE))
 
 
 class StudentManager(models.Manager):
@@ -43,25 +43,27 @@ class StudentManager(models.Manager):
 class Student(models.Model):
 
     class AcademicChoices(models.IntegerChoices):
-        YES = 1 , _(vn_identity.YES)
-        NO = 2 , _(vn_identity.NO)
+        YES = 1 , (vn_identity.YES)
+        NO = 2 , (vn_identity.NO)
     
     class EntryChoices(models.IntegerChoices):
-        YES = 1 , _(vn_identity.YES)
-        NO = 2 , _(vn_identity.NO)
+        YES = 1 , (vn_identity.YES)
+        NO = 2 , (vn_identity.NO)
 
-    user_id = models.ForeignKey(get_user_model() , on_delete=models.PROTECT , verbose_name = _(vn_identity.USER_ID))
-    entry_year = jmodels.jDateField(verbose_name=_(vn_identity.ENTRY_YEAR))
-    entry_term= models.IntegerField(choices = EntryChoices.choices , verbose_name=_(vn_identity.ENTRY_TERM))
-    average = models.models.FloatField(verbose_name=_(vn_identity.AVERAGE))
-    academic_year= models.IntegerField(choices = AcademicChoices.choices , verbose_name=_(vn_identity.ACADEMIC_YEAR))
-    current_term = models.ForeignKey("EduTerm", on_delete=models.PROTECT, verbose_name=_(vn_identity.CURRENT_TERM))
+    user = models.ForeignKey(get_user_model() , on_delete=models.PROTECT , verbose_name = (vn_identity.USER_ID))
+    entry_year = jmodels.jDateField(verbose_name=(vn_identity.ENTRY_YEAR))
+    entry_term= models.IntegerField(choices = EntryChoices.choices , verbose_name=(vn_identity.ENTRY_TERM))
+    average = models.models.FloatField(verbose_name=(vn_identity.AVERAGE))
+    academic_year= models.IntegerField(choices = AcademicChoices.choices , verbose_name=(vn_identity.ACADEMIC_YEAR))
+    current_term = models.ForeignKey("EduTerm", on_delete=models.PROTECT, verbose_name=(vn_identity.CURRENT_TERM))
 
 class TeacherManager(models.Manager):
     pass
 
 
 class Teacher(models.Model):
-    user_id = models.ForeignKey(get_user_model() , on_delete=models.PROTECT , verbose_name = _(vn_identity.USER_ID))
-    level = models.CharField(verbose_name=_(vn_identity.LEVEL), max_length=64)
-    expert = models.CharField(verbose_name=_(vn_identity.EXPERT), max_length=64)
+    user = models.ForeignKey(get_user_model() , on_delete=models.PROTECT , verbose_name = (vn_identity.USER_ID))
+    level = models.CharField(verbose_name=(vn_identity.LEVEL), max_length=64)
+    expert = models.CharField(verbose_name=(vn_identity.EXPERT), max_length=64)
+
+
--- Identity/variable_names.py
+++ Identity/variable_names.py
@@ -1,33 +1,36 @@
+from django.utils.translation import gettext_lazy as _
+
 # User Model
-PORTRAIT = "عکس پروفایل"
-MOBILE = "شماره موبایل"
-GENDER = "جنسیت"
-BIRTH_DATE = "تاریخ تولد"
-NATIONAL_CODE = "کد ملی"
-MILITARY_SERVICE = "وضعیت نظام وظیفه"
-COLLEGE_ID = "دانشکده"
-EDU_FIELD_ID = "رشته تحصیلی"
-IS_IT_MANAGER = "مدیر آی است"
-IS_CHANCELLOR = "معاون آموزشی است"
-IS_STUDENT = "دانشجو است"
-IS_TEACHER = "استاد است"
+PORTRAIT = _("عکس پروفایل")
+MOBILE = _("شماره موبایل")
+GENDER = _("جنسیت")
+BIRTH_DATE = _("تاریخ تولد")
+NATIONAL_CODE = _("کد ملی")
+MILITARY_SERVICE = _("وضعیت نظام وظیفه")
+COLLEGE_ID = _("دانشکده")
+EDU_FIELD_ID = _("رشته تحصیلی")
+IS_IT_MANAGER = _("مدیر آی است")
+IS_CHANCELLOR = _("معاون آموزشی است")
+IS_STUDENT = _("دانشجو است")
+IS_TEACHER = _("استاد است")
 
 # Student Model
-USER_ID = "آیدی کاربر"
-ENTRY_YEAR = "سال ورود"
-ENTRY_TERM = "اولین ترم"
-AVERAGE = "میانگین معدل"
-ACADEMIC_YEAR = "سنوات"
-CURRENT_TERM = "ترم کنونی"
+USER_ID = _("آیدی کاربر")
+ENTRY_YEAR = _("سال ورود")
+ENTRY_TERM = _("اولین ترم")
+AVERAGE = _("میانگین معدل")
+ACADEMIC_YEAR = _("سنوات")
+CURRENT_TERM = _("ترم کنونی")
 
 
 # Teacher Model
-LEVEL = "سطح" 
-EXPERT = "تجربه"
+LEVEL = _("سطح")
+EXPERT = _("تجربه")
 
-YES = "بله"
-NO = "نه"
-MALE = "مرد"
-FEMALE ="زن"
-DUTIABLE = "مشول به خدمت"
-EXEMPT = "معاف از خدمت"
+# Choices
+YES = _("بله")
+NO = _("نه")
+MALE = _("مرد")
+FEMALE = _("زن")
+DUTIABLE = _("مشول به خدمت")
+EXEMPT = _("معاف از خدمت")
