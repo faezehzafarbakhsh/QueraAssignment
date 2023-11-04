deb1706983c8b7edecd7155eba74ebed84482142
Ali Safari <alisfryly075@gmail.com>
2023-11-04 14:00:04 +0330

    [feature] wrote identity.views

--- Identity/views.py
+++ Identity/views.py
@@ -1,3 +1,64 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# User Endpoints
+
+
+class user_authentication_view():
+    pass
+
+
+class user_change_password_view():
+    pass
+
+# It Manager Teachers Endpoints
+
+
+class teachers_retrieve_or_create_view():
+    pass
+
+
+class teachers_update_or_destroy_view():
+    pass
+
+# It Manager Students Endpoints
+
+
+class students_retrieve_or_create_view():
+    pass
+
+
+class students_update_or_destroy_view():
+    pass
+
+# It Manager Chancellor Endpoints
+
+
+class chancellor_retrieve_or_create_view():
+    pass
+
+
+class chancellor_update_or_destroy_view():
+    pass
+
+# It Manager College Endpoints
+
+
+class college_retrieve_or_create_view():
+    pass
+
+
+class college_update_or_destroy_view():
+    pass
+
+# It Manager Term Endpoints
+
+
+class term_retrieve_or_create_view():
+    pass
+
+
+class term_update_or_destroy_view():
+    pass
