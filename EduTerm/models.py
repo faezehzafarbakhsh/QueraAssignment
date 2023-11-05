443065bb4537cf0c5c780cc2e7b4133a43b9d0d2
arman <cheraghvandiarman@gamil.com>
2023-11-05 20:04:51 +0430

    [fix](1) models and variable_names

--- EduTerm/models.py
+++ EduTerm/models.py
@@ -1,7 +1,7 @@
 from django.db import models 
 from django_jalali.db import models as jmodels
-from variable_names import *
-from django.utils.translation import gettext_lazy as _
+import variable_names as vn_EduTerm
+
 
 '''
 TERM MODEL
@@ -9,26 +9,26 @@ TERM MODEL
 class TermManager(models.manager):
     pass
 class Term(models.Model):
-    name = models.CharField(max_length=64,verbose_name=_(Name))
-    enrollment_start_datetime = jmodels.jdatetime(verbose_name=_(Enrollment_start_datetime))
-    enrollment_end_datetime = jmodels.jDateField(verbose_name=_(Enrollment_end_datetime))
-    class_start_datetime = jmodels.jDateField(verbose_name=_(Class_start_datetime))
-    class_end_datetime = jmodels.jDateField(verbose_name=_(Class_end_datetime))
-    modify_start_datetime = jmodels.jDateField(verbose_name=_(Modify_start_datetime))
-    modify_end_datetime = jmodels.jDateField(verbose_name=_(Modify_end_datetime))
-    emergency_course_drop_end_datetime = jmodels.jDateField(verbose_name=_(Emergency_course_drop_end_datetime))
-    exam_start_date =  jmodels.jDateField(verbose_name=_(Exam_start_date))
-    term_end_date =  jmodels.jDateField(verbose_name=_(Term_end_date))
+    name = models.CharField(max_length=64,verbose_name=vn_EduTerm.NAME)
+    enrollment_start_datetime = jmodels.jdatetime(verbose_name=vn_EduTerm.ENROLLMENT_START_DATETIME)
+    enrollment_end_datetime = jmodels.jdatetime(verbose_name=vn_EduTerm.ENROLLMENT_END_DATETIME)
+    class_start_datetime = jmodels.jdatetime(verbose_name=vn_EduTerm.CLASS_START_DATETIME)
+    class_end_datetime = jmodels.jdatetime(verbose_name=vn_EduTerm.CLASS_END_DATETIME)
+    modify_start_datetime = jmodels.jdatetime(verbose_name=vn_EduTerm.MODIFY_START_DATETIME)
+    modify_end_datetime = jmodels.jdatetime(verbose_name=vn_EduTerm.MODIFY_END_DATETIME)
+    emergency_course_drop_end_datetime = jmodels.jdatetime(verbose_name=vn_EduTerm.EMERGENCY_COURSE_DROP_END_DATETIME)
+    exam_start_date =  jmodels.jDateField(verbose_name=vn_EduTerm.EXAM_START_DATE)
+    term_end_date =  jmodels.jDateField(verbose_name=vn_EduTerm.TERM_END_DATE)
 
 
 class CourseTermManager(models.manager):
     pass
 class CourseTerm(models.Model):
-    course=models.ForeignKey("EduBase.Course", verbose_name=_(Course), on_delete=models.PROTECT)
-    term=models.ForeignKey("EduTerm", verbose_name=_(Term), on_delete=models.PROTECT)
-    class_day=jmodels.jdatetime(verbose_name=_(Class_day))
-    class_time=models.DateTimeField(timezone=True,verbose_name=_(Class_time))
-    exam_datetime=jmodels.jDateField(verbose_name=_(Exam_datetime))
-    exam_place=models.CharField(max_length=128,verbose_name=_(Exam_place))
-    teacher=models.ForeignKey("Identity.User", verbose_name=_(Teacher), on_delete=models.PROTECT)
-    capacity=models.IntegerField(verbose_name=_(Capacity))
+    course=models.ForeignKey("EduBase.Course", verbose_name=vn_EduTerm.COURSE, on_delete=models.PROTECT)
+    term=models.ForeignKey("EduTerm.Term", verbose_name=vn_EduTerm.TERM, on_delete=models.PROTECT)
+    class_day=jmodels.jDateField(verbose_name=vn_EduTerm.CLASS_DAY)
+    class_time=jmodels.jdatetime(verbose_name=vn_EduTerm.CLASS_TIME)
+    exam_datetime=jmodels.jdatetime(verbose_name=vn_EduTerm.EXAM_DATETIME)
+    exam_place=models.CharField(max_length=128,verbose_name=vn_EduTerm.EXAM_PLACE)
+    teacher=models.ForeignKey("Identity.User", verbose_name=vn_EduTerm.TEACHER, on_delete=models.PROTECT)
+    capacity=models.IntegerField(verbose_name=vn_EduTerm.CAPACITY)
--- EduTerm/variable_names.py
+++ EduTerm/variable_names.py
@@ -1,24 +1,25 @@
+from django.utils.translation import gettext_lazy as _
 #TERM
-Name='اسم'
-Enrollment_start_datetime='تاریخ شروع انتخاب واحد'
-Enrollment_end_datetime='تاریخ پایان انتخاب واحد'
-Class_start_datetime ='تاریخ شروع کلاس ها'
-Class_end_datetime='تاریخ پایان کلاس ها'
-Modify_start_datetime='تاریخ شروع حذف واضافه'
-Modify_end_datetime='تاریخ پایان حذف و اضافه'
-Emergency_course_drop_end_datetime='زمان حذف اضطراری'
-Exam_start_date=' تاریخ شروع امتحان ها'
-Term_end_date='پایان ترم'
+NAME='اسم'
+ENROLLMENT_START_DATETIME=_('تاریخ شروع انتخاب واحد')
+ENROLLMENT_END_DATETIME=_('تاریخ پایان انتخاب واحد')
+CLASS_START_DATETIME =_('تاریخ شروع کلاس ها')
+CLASS_END_DATETIME=_('تاریخ پایان کلاس ها')
+MODIFY_START_DATETIME=_('تاریخ شروع حذف واضافه')
+MODIFY_END_DATETIME=_('تاریخ پایان حذف و اضافه')
+EMERGENCY_COURSE_DROP_END_DATETIME=_('زمان حذف اضطراری')
+EXAM_START_DATE=_(' تاریخ شروع امتحان ها')
+TERM_END_DATE=_('پایان ترم')
 
 
 #CourseTerm
-Course='درس'
-Term='ترم'
-Class_day='روز کلاس'
-Class_time='زمان کلاس'
-Exam_datetime='تاریخ امتحان'
-Exam_place='مکان امتحان'
-Teacher='استاد'
-Capacity='ظرفیت'
+COURSE=_('درس')
+TERM=_('ترم')
+CLASS_DAY=_('روز کلاس')
+CLASS_TIME=_('زمان کلاس')
+EXAM_DATETIME=_('تاریخ امتحان')
+EXAM_PLACE=_('مکان امتحان')
+TEACHER=_('استاد')
+CAPACITY=_('ظرفیت')
 
 
