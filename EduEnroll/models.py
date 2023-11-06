447352d8575e90c0b308f45fbe674a7be6ddbdd9
darwvin <darwvin@hotmail.com>
2023-11-06 22:15:28 +0330

    add EduEnroll Model

--- EduEnroll/models.py
+++ EduEnroll/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Enrollment(models.Model):
+    """ 
+    """
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+    )
+    id = models.AutoField(primary_key=True, unique=True)
+    term = models.ForeignKey('Term')
+    student = models.ForeignKey('Student')
+    teacher_assistant = models.ForeignKey('Teacher')
+    status = models.CharField(choices=STATUS_CHOICES)
+    teacher_assistant_description = models.TextField()
+    taken_term_number = models.IntegerField()
+
+class StudentCourse(models.Model):
+    """درسی که دانشجو برداشته"""
+    STATUS_CHOICES = (
+        ('enrolled', 'Enrolled'),
+        ('deleted', 'Deleted'),
+        ('completed', 'Completed'),
+    )
+    id = models.AutoField(primary_key=True)
+    course_term = models.ForeignKey('CourseTerm')
+    student = models.ForeignKey("Student")
+    status = models.CharField(choices=STATUS_CHOICES)
+    score = models.FloatField()
