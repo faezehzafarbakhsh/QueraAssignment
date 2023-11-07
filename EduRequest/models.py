ed1024af91d8f61f9c26ff556b9c58ffb7c9d67b
Afshin <khodeafshin@gmail.com>
2023-11-07 22:12:08 +0330

    [modify] fix models

--- EduRequest/models.py
+++ EduRequest/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import variable_names as vn_edurequest
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
 
 # Create your models here.
@@ -12,17 +13,25 @@ class StudentRequestManager(models.Manager):
 class StudentRequest(models.Model):
 
     class StatusChoices(models.IntegerChoices):
-        pass
+        ACCEPT = _('پذیرش')
+        DENIED = _('رد شده')
+        IN_PROGRESS = _('در حال بررسی')
+        
 
     class RequestTypeChoices(models.IntegerChoices):
-        pass
+        Late_Add_drop = _('درخواست حذف و اضافه')
+        EMPLOYMENT_IN_EDUCATION = _('درخواست اشتغال به تحصیل')
+        REGISTREING = _("درخواست ثبت نام")
+        APPEAL = _('درخواست تجدید نظر')
+        DELETE_STUDENT_SEMESTER = _('درخواست حذف ترم')
+        STUDENT_EMERGENCY_REMOVAL = _("درخواست حذف اضطراری دانشجو")
 
-    student = models.ForeignKey('Identity.User', on_delete=models.PROTECT,
+    student = models.ForeignKey(get_user_model(), on_delete=models.PROTECT,
                                 verbose_name=vn_edurequest.STUDENT_REQUEST_STUDENT)
     term = models.ForeignKey('EduTerm.Term', on_delete=models.PROTECT,
                              verbose_name=vn_edurequest.STUDENT_REQUEST_TERM)
     course_term = models.ForeignKey(
-        'EduTerm.CourceTerm', on_delete=models.PROTECT, verbose_name=vn_edurequest.STUDENT_REQUEST_COURSE_TERM)
+        'EduTerm.CourseTerm', on_delete=models.PROTECT, verbose_name=vn_edurequest.STUDENT_REQUEST_COURSE_TERM)
     request_description = models.TextField(
         verbose_name=vn_edurequest.STUDENT_REQUEST_REQUEST_DESCRIPTION)
     answer = models.TextField(verbose_name=vn_edurequest.STUDENT_REQUEST_ANSWER)
@@ -31,7 +40,7 @@ class StudentRequest(models.Model):
     has_academic_year = models.BooleanField(
         default=False, verbose_name=vn_edurequest.STUDENT_REQUEST_HAS_ACADEMIC_YEAR)
     user_answer = models.ForeignKey(
-        'Identy.User', on_delete=models.PROTECT, user_answer=vn_edurequest.STUDENT_REQUEST_USER_ANSWER)
+        get_user_model(), on_delete=models.PROTECT, user_answer=vn_edurequest.STUDENT_REQUEST_USER_ANSWER)
     request_type = models.IntegerField(
         choices=RequestTypeChoices.choices, verbose_name=vn_edurequest.STUDENT_REQUEST_REQUEST_TYPE)
 
@@ -41,7 +50,7 @@ class EnrollmentCertificateManager(models.Manager):
 
 
 class EnrollmentCertificate(models.Model):
-    student = models.ForeignKey('Identity.User', on_delete=models.PROTECT,
+    student = models.ForeignKey(get_user_model(), on_delete=models.PROTECT,
                                 verbose_name=vn_edurequest.ENROLLMENTCERTIFICATE_STUDENT)
     term = models.ForeignKey('EduTerm.Term', on_delete=models.PROTECT,
                              verbose_name=vn_edurequest.ENROLLMENTCERTIFICATE_TERM)
