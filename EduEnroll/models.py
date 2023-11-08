8f21d3f844e0c8f3308df9d7b909e46d3e9413cd
darwvin <darwvin@hotmail.com>
2023-11-08 20:20:37 +0330

    [add] EduEnroll Model

--- EduEnroll/models.py
+++ EduEnroll/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import variable_names as translate
 
-# Create your models here.
+class Enrollment(models.Model):
+    STATUS_CHOICES = (
+        (translate.PENDING, translate.PENDING),
+        (translate.APPROVED, translate.APPROVED),
+        (translate.REJECTED, translate.REJECTED),
+    )
+    id = models.AutoField(primary_key=True, unique=True)
+    term = models.ForeignKey('Term', verbose_name=translate.TERM)
+    student = models.ForeignKey('Student', verbose_name=translate.STUDENT)
+    teacher_assistant = models.ForeignKey('Teacher', verbose_name=translate.TEACHERASISTANT)
+    status = models.CharField(choices=STATUS_CHOICES, verbose_name=translate.STATUS)
+    teacher_assistant_description = models.TextField(verbose_name=translate.TEACHER_ASISTANT_DESCRIPTION)
+    taken_term_number = models.IntegerField(verbose_name=translate.TAKEN_TERM_NUMBER)
+
+class StudentCourse(models.Model):
+    STATUS_CHOICES = (
+        (translate.ENROLLED, translate.ENROLLED),
+        (translate.DELETED, translate.DELETED),
+        (translate.COMPLETED, translate.COMPLETED),
+    )
+    id = models.AutoField(primary_key=True)
+    course_term = models.ForeignKey('CourseTerm', verbose_name=translate.COURSE_TERM)
+    student = models.ForeignKey("Student", verbose_name=translate.STUDENT)
+    status = models.CharField(choices=STATUS_CHOICES, verbose_name=translate.STATUS)
+    score = models.FloatField(verbose_name=translate.SCORE, min=0, max=100)
---
+++ EduEnroll/variable_names.py
@@ -0,0 +1,19 @@
+from django.utils.translation import gettext_lazy as _
+
+#Enrollment
+PENDING = _("انتظار")
+APPROVED = _("تایید شده")
+REJECTED = _("رد شد")
+TERM = _("ترم")
+STUDENT = _("دانشجو")
+TEACHER_ASISTANT = _("استادیار")
+STATUS = _("وضعیت")
+TEACHER_ASISTANT_DESCRIPTION = _("توضیحات استادیار")
+TAKEN_TERM_NUMBER = _("توکن ترم")
+
+#StudentCourse
+ENROLLED = _("ثبت نام کرده")
+DELETED = _("حذف شده")
+COMPLETED = _("کامل شده")
+COURSE_TERM = _("درس ترم")
+SCORE = _("نمره")
