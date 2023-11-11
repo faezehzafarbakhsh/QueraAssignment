a2a25219e4ffdbd492f17c81cb855a1dd0e16ec1
arman <cheraghvandiarman@gamil.com>
2023-11-11 16:40:32 +0430

    [add]eduterm_views.py(1)

--- EduTerm/views.py
+++ EduTerm/views.py
@@ -4,11 +4,34 @@ from rest_framework.permissions import AllowAny
 from EduTerm import serializers as edu_term_serializers
 from EduTerm import models as edu_term_models
 # Create your views here.
-
+'''
+e. اندپوینتهای ترم و دروس قابل اخذ
+هر دانشجو و استاد میتواند با استفاده از URL های زیر لیست و جزییات
+ترمها را مشاهده کند.
+'''
 
 class TermListCreateView(generics.ListCreateAPIView):
     serializer_class = edu_term_serializers.TermSerializer
     queryset =edu_term_models.Term.objects.all()
     http_method_names = [ 'get','post', ]
     permission_classes = (AllowAny,)
-    
+
+class TermRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = edu_term_serializers.TermSerializer
+    queryset = edu_term_models.Term.objects.all()
+    http_method_names = ['get', 'put', 'delete',]
+    permission_classes = (AllowAny,)
+
+
+class CoursetermFieldListCreateView(generics.ListCreateAPIView):
+    serializer_class = edu_term_serializers.CourseTermSerializer
+    queryset = edu_term_models.CourseTerm.objects.all()
+    http_method_names = ['get', 'post',]
+    permission_classes = (AllowAny,)
+
+
+class  CoursetermRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = edu_term_serializers.CourseTermSerializer
+    queryset = edu_term_models.CourseTerm.objects.all()
+    http_method_names = ['get', 'put', 'delete',]
+    permission_classes = (AllowAny,)
