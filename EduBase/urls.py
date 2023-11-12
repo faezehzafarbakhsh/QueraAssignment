e31b10c7a93c02547414c3d0ae2a179a74556293
faezehzafarbakhsh <faezehzafarbakhsh@gmail.com>
2023-11-12 21:46:01 +0330

    [modify] wrote view and serializer and url of edu_base app

--- EduBase/urls.py
+++ EduBase/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('admin/college/<int:pk>/', edu_base_views.CollegeRetrieveUpdateDestroyView.as_view(), name='college_retrieve_update_destroy_view'),
     path('admin/edu_field/', edu_base_views.EduFieldListCreateView.as_view(), name='edu_field_list_create_view'),
     path('admin/edu_field/<int:pk>/', edu_base_views.EduFieldRetrieveUpdateDestroyView.as_view(), name='edu_field_retrieve_update_destroy_view'),
+    path('admin/course_field/', edu_base_views.CourseListCreateView.as_view(), name='course_list_create_view'),
+    path('admin/course_field/<int:pk>/', edu_base_views.CourseRetrieveUpdateDestroyView.as_view(), name='course_retrieve_update_destroy_view'),
+    path('admin/course_relation_field/', edu_base_views.CourseRelationListCreateView.as_view(), name='course_relation_list_create_view'),
+    path('admin/course_relation_field/<int:pk>/', edu_base_views.CourseRelationRetrieveUpdateDestroyView.as_view(), name='course_relation_retrieve_update_destroy_view'),
+
 ]
--- EduBase/views.py
+++ EduBase/views.py
@@ -1,23 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
-
 from EduBase import serializers as edu_base_serializers
 from EduBase import models as edu_base_models
-
-
-class CollegeListCreateView(generics.ListCreateAPIView):
-    serializer_class = edu_base_serializers.CollegeSerializer
-    queryset = edu_base_models.College.objects.all()
-    http_method_names = ['get', 'post']
-    permission_classes = (AllowAny,)
-
-
-class CollegeRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = edu_base_serializers.CollegeSerializer
-    queryset = edu_base_models.College.objects.all()
-    http_method_names = ['get', 'put', 'delete']
-    permission_classes = (AllowAny,)
 
 
 class EduFieldListCreateView(generics.ListCreateAPIView):
@@ -34,5 +19,44 @@ class EduFieldRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (AllowAny,)
 
 
-class CourseListView():
-    pass
+class CourseListCreateView(generics.ListCreateAPIView):
+    serializer_class = edu_base_serializers.CourseSerializer
+    queryset = edu_base_models.Course.objects.all()
+    http_method_names = ['get', 'post']
+    permission_classes = (AllowAny,)
+
+
+class CourseRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = edu_base_serializers.CourseSerializer
+    queryset = edu_base_models.Course.objects.all()
+    http_method_names = ['get', 'put', 'delete']
+    permission_classes = (AllowAny,)
+
+
+class CourseRelationListCreateView(generics.ListCreateAPIView):
+    serializer_class = edu_base_serializers.CourseRelationSerializer
+    queryset = edu_base_models.CourseRelation.objects.all()
+    http_method_names = ['get', 'post']
+    permission_classes = (AllowAny,)
+
+
+class CourseRelationRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = edu_base_serializers.CourseRelationSerializer
+    queryset = edu_base_models.Course.objects.all()
+    http_method_names = ['get', 'put', 'delete']
+    permission_classes = (AllowAny,)
+
+
+class CollegeListCreateView(generics.ListCreateAPIView):
+    serializer_class = edu_base_serializers.CollegeSerializer
+    queryset = edu_base_models.College.objects.all()
+    http_method_names = ['get', 'post']
+    permission_classes = (AllowAny,)
+
+
+class CollegeRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = edu_base_serializers.CollegeSerializer
+    queryset = edu_base_models.College.objects.all()
+    http_method_names = ['get', 'put', 'delete']
+    permission_classes = (AllowAny,)
+
