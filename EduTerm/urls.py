379bf91f08babd60a47959f16435bb9707aa23be
arman <cheraghvandiarman@gamil.com>
2023-11-14 19:25:41 +0430

    [modify]

--- EduTerm/urls.py
+++ EduTerm/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from EduTerm import views as edu_term_views
 urlpatterns = [
 
 ]
--- EduTerm/views.py
+++ EduTerm/views.py
@@ -4,18 +4,14 @@ from rest_framework.permissions import AllowAny
 from EduTerm import serializers as edu_term_serializers
 from EduTerm import models as edu_term_models
 # Create your views here.
-'''
-e. اندپوینتهای ترم و دروس قابل اخذ
-هر دانشجو و استاد میتواند با استفاده از URL های زیر لیست و جزییات
-ترمها را مشاهده کند.
-'''
+
 
 class TermListCreateView(generics.ListCreateAPIView):
     serializer_class = edu_term_serializers.TermSerializer
     queryset =edu_term_models.Term.objects.all()
     http_method_names = [ 'get','post', ]
     permission_classes = (AllowAny,)
-
+    
 class TermRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = edu_term_serializers.TermSerializer
     queryset = edu_term_models.Term.objects.all()
