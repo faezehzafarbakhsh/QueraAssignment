392a6d28c2d66545274d32500bf7714530aade67
Ali Safari <alisfryly075@gmail.com>
2023-11-17 14:49:31 +0330

    [add] permissons

--- EduBase/urls.py
+++ EduBase/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from EduBase import views as edu_base_views
 urlpatterns = [
     path('admin/college/', edu_base_views.CollegeListCreateView.as_view(), name='college_list_create_view'),
     path('admin/college/<int:pk>/', edu_base_views.CollegeRetrieveUpdateDestroyView.as_view(), name='college_retrieve_update_destroy_view'),
