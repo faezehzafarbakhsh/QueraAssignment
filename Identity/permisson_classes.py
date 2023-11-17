dbe391f605e0bc3bc22145ccc5d2c731a3db5118
Ali Safari <alisfryly075@gmail.com>
2023-11-17 13:45:23 +0330

    [add] permissons

--- Identity/permisson_classes.py
+++ Identity/permisson_classes.py
@@ -8,3 +8,30 @@ class IsItManager(BasePermission):
 
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_it_manager
+
+
+class IsStudent(BasePermission):
+    """
+    Custom permission to check if the user is an Student.
+    """
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_student
+
+
+class IsTeacher(BasePermission):
+    """
+    Custom permission to check if the user is an Teacher.
+    """
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_teacher
+
+
+class IsChancellor(BasePermission):
+    """
+    Custom permission to check if the user is an Chancellor.
+    """
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_chancellor
