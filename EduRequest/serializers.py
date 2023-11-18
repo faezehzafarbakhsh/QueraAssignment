45aff19df563759900cfc3fe3e5372dad84b1eb8
Ali Safari <alisfryly075@gmail.com>
2023-11-18 22:59:11 +0330

    [add] add all students requests

--- EduRequest/serializers.py
+++ EduRequest/serializers.py
@@ -2,16 +2,18 @@ from rest_framework import serializers
 from EduRequest import models as edu_request_models
 from EduTerm import models as edu_term_models
 
+
 class StudentRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = edu_request_models.StudentRequest
-        fields = ['request_description',]
-        
+        fields = ['request_description', 'request_type']
+        extra_kwargs = {'request_type': {'read_only': True}}
+
 
 class EnrollmentCertificateSerializer(serializers.ModelSerializer):
     model = edu_request_models.EnrollmentCertificate
-    fields = ['student' , 'term' , 'enrollment_certificate_place',]
-    
+    fields = ['student', 'term', 'enrollment_certificate_place',]
+
 
 class TeacherAnswerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,23 +21,14 @@ class TeacherAnswerSerializer(serializers.ModelSerializer):
         fields = ['status', 'request_description', 'answer']
 
 
-
-
-
-
-
-
 # class  EDUTERM_CourseSerializer(serializers.ModelSerializer):
 #     class Meta:
 #         model = edu_term_models.CourseTerm
 #         fields = ['course' , ]
-    
+
 
 # class AppealAgainstCourseSerializer(serializers.ModelSerializer):
 #     course = EDUTERM_CourseSerializer()
 #     class Meta:
 #         model = edu_request_models.StudentRequest
 #         fields = ['student' , 'course' , 'request_description' , 'answer', ]
-        
-        
-
