17e34d95fe843ced35f0cc09a28ef7b1bd79e2e2
Ali Safari <alisfryly075@gmail.com>
2023-11-22 12:38:50 +0330

    [add]

--- EduRequest/urls.py
+++ EduRequest/urls.py
@@ -3,8 +3,8 @@ from EduRequest import views as edu_request_views
 
 urlpatterns = [
     # Enrollment
-    path('admin/enrollment_certificate/' , edu_request_views.EnrollmentCertificateListCreateView.as_view() , name='enrollment_certificate_list_create_view'),
-    path('admin/enrollment_certificate/<int:pk>/' , edu_request_views.EnrollmentCertificateRetrieveUpdateDestroyView.as_view() , name='enrollment_certificate_retrieve_update_destroy_view'),
+    path('student/enrollment-certificate/' , edu_request_views.EnrollmentCertificateListCreateView.as_view() , name='enrollment_certificate_list_create_view'),
+    path('student/enrollment-certificate/<int:pk>/' , edu_request_views.EnrollmentCertificateRetrieveUpdateDestroyView.as_view() , name='enrollment_certificate_retrieve_update_destroy_view'),
     
     # Student Request
     path('student/me/course_term/<int:course_term_pk>/<int:request_type>/request/',
