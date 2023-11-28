073b7f70dbb2bc002afd990c01298fa0be5fdc42
Ali Safari <alisfryly075@gmail.com>
2023-11-28 19:48:58 +0330

    sadfegtr

--- Identity/admin.py
+++ Identity/admin.py
@@ -6,7 +6,7 @@ from django.http.request import HttpRequest
 
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ( 'username', 'email', 'first_name', 'last_name',
+    list_display = ('username', 'email', 'first_name', 'last_name',
                     'gender', 'mobile', 'is_student', 'is_teacher', 'is_chancellor', 'is_it_manager')
     list_filter = ('username', 'email', 'first_name', 'last_name', 'gender',
                    'mobile', 'is_student', 'is_teacher', 'is_chancellor', 'is_it_manager')
@@ -16,7 +16,7 @@ class UserAdmin(admin.ModelAdmin):
 
 @admin.register(models.Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ( 'user', 'entry_year',
+    list_display = ('user', 'entry_year',
                     'entry_term', 'current_term', 'average', 'academic_year')
     list_filter = ('user', 'entry_year', 'entry_term',
                    'current_term', 'average', 'academic_year')
@@ -38,7 +38,7 @@ class StudentAdmin(admin.ModelAdmin):
 @admin.register(models.Teacher)
 class TeacherAdmin(admin.ModelAdmin):
     list_filter = ('user', 'level', 'expert')
-    list_filter = ('user__unique_code','user', 'level', 'expert')
+    list_filter = ('user__unique_code', 'user', 'level', 'expert')
     search_fields = ('user__username', 'user__first_name', 'user__last_name',
                      'user__email', 'user__ national_code', 'level', 'expert')
 
