eeb85d28d61d5e5d4f398e093641b72c186fc57a
Ali Safari <alisfryly075@gmail.com>
2023-11-28 19:41:48 +0330

    adfgfs

--- Identity/admin.py
+++ Identity/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from . import models
-from typing import Any
 from django.db.models.query import QuerySet
 from django.http.request import HttpRequest
 
 
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'gender',
-                    'mobile', 'is_student', 'is_teacher', 'is_chancellor', 'is_it_manager')
+    list_display = ('unique_code', 'username', 'email', 'first_name', 'last_name',
+                    'gender', 'mobile', 'is_student', 'is_teacher', 'is_chancellor', 'is_it_manager')
     list_filter = ('username', 'email', 'first_name', 'last_name', 'gender',
                    'mobile', 'is_student', 'is_teacher', 'is_chancellor', 'is_it_manager')
     search_fields = ('username', 'email', 'first_name', 'last_name', 'gender',
@@ -17,14 +16,17 @@ class UserAdmin(admin.ModelAdmin):
 
 @admin.register(models.Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'entry_year', 'entry_term',
-                    'current_term', 'average', 'academic_year')
+    list_display = ('unique_code', 'user', 'entry_year',
+                    'entry_term', 'current_term', 'average', 'academic_year')
     list_filter = ('user', 'entry_year', 'entry_term',
                    'current_term', 'average', 'academic_year')
     search_fields = ('user__username', 'user__first_name', 'user__last_name', 'user__email',
                      'user__ national_code', 'entry_year', 'entry_term', 'current_term', 'average', 'academic_year')
 
-    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+    def unique_code(self, obj):
+        return obj.user.unique_code
+
+    def get_queryset(self, request: HttpRequest):
         qs = super().get_queryset(request)
         user = request.user
         chancellor_college = user.college
@@ -35,12 +37,15 @@ class StudentAdmin(admin.ModelAdmin):
 
 @admin.register(models.Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    list_display = ('user', 'level', 'expert')
-    list_filter = ('user', 'level', 'expert')
+    list_filter = ('unique_code','user', 'level', 'expert')
+    list_filter = ('user__unique_code','user', 'level', 'expert')
     search_fields = ('user__username', 'user__first_name', 'user__last_name',
                      'user__email', 'user__ national_code', 'level', 'expert')
 
-    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+    def unique_code(self, obj):
+        return obj.user.unique_code
+
+    def get_queryset(self, request: HttpRequest):
         qs = super().get_queryset(request)
         user = request.user
         chancellor_college = user.college
