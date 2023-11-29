4a6412da041d8aa8606048e76d5da829e1ba90bf
Ali Safari <alisfryly075@gmail.com>
2023-11-29 23:37:47 +0330

    [fix]

--- QueraPyRate/urls.py
+++ QueraPyRate/urls.py
@@ -28,9 +28,6 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/', include('Identity.urls')),
     path('api/', include('EduBase.urls')),
     path('api/', include('EduTerm.urls')),
