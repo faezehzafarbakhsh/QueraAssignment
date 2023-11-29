89a8ad9161565872642ae38f7ba409c1d67bb5e3
faezehzafarbakhsh <faezehzafarbakhsh@gmail.com>
2023-11-29 22:47:55 +0330

    Fix conflict

--- QueraPyRate/settings.py
+++ QueraPyRate/settings.py
@@ -198,7 +198,7 @@ DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION':'redis://localhost:6379/1',
+        'LOCATION':'redis://redis:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
