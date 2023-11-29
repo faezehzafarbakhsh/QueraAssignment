a1006f4b4ec30763cf8d0f85fc7636054888dec3
Ali Safari <alisfryly075@gmail.com>
2023-11-29 23:34:56 +0330

    [fix]

--- EduTerm/filters.py
+++ EduTerm/filters.py
@@ -3,9 +3,9 @@ from EduTerm import models as eduterm_models
 
 
 
-class Termfilters(filters.filterset):
+class TermFilters(filters.FilterSet):
     class Meta:
         model=eduterm_models.Term
-        fields=['name','exam_start_date','term_end_date',' active_term', ]
+        fields=['name','exam_start_date','term_end_date', ]
         
 
